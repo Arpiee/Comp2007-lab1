dcef8085f85620d5829e7a2132ec669193699a86
Arpiee <arpit.kohli01@gmail.com>
2018-02-13 01:48:55 -0500

    first commit

---
+++ Lab-1/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Store/Details?samsung=[value]
+
+        public ActionResult Details(string samsung)
+        {
+            // check for an empty samsung
+            if (samsung == null)
+            {
+                //return View("Error");
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Samsung = samsung;
+            return View(); 
+        }
+    }
+}
